5674f7327256e7e3dea608b7ccd828a240b11d45
Rscime <Rachelscime@gmail.com>
2023-05-24 21:27:15 -0400

    added form for City location that saves to local storage... Create form in html

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,3 @@
-//saving location input into local storage Rscime
-var locationBtn = $('#searchBtn');
-locationBtn.on('click', function(e){
-    e.preventDefault();
-    let locationText  = $('#locationInfo').val();
-    //stores input location (locationText) under the key 'locationKey'
-    localStorage.setItem('locationKey', locationText);
-    console.log(locationText);
-})
-
 var APIkey = "bdAaYSOFUtzpTl31szeCTwxkqTQkbXkPhqVSXr3O"; 
 
 async function getExchangeRate(fromCurrency, toCurrency) {
@@ -37,3 +27,13 @@ async function getExchangeRate(fromCurrency, toCurrency) {
   
 
   main();
+
+  //saving location input into local storage Rscime
+var locationBtn = $('#searchBtn');
+locationBtn.on('click', function(e){
+    e.preventDefault();
+    let locationText  = $('#locationInfo').val();
+    //stores input location (locationText) under the key 'locationKey'
+    localStorage.setItem('locationKey', locationText);
+    console.log(locationText);
+})
