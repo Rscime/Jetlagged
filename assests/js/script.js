00e22ec87ce26eef7a40b2deac7f59616544e841
Karen <kavictoria2910@hotmail.com>
2023-05-25 20:16:52 -0400

    Save button

--- assests/js/script.js
+++ assests/js/script.js
@@ -37,3 +37,26 @@ locationBtn.on('click', function(e){
     localStorage.setItem('locationKey', locationText);
     console.log(locationText);
 })
+
+// Victoria 
+document.addEventListener("DOMContentLoaded", function() {
+  var saveButtons = document.querySelectorAll(".saveButton");
+  var textAreas = document.querySelectorAll("textarea");
+
+  // Taking the saved text to local storage 
+  textAreas.forEach(function(textArea, index) {
+    var savedText = localStorage.getItem("savedText_" + index);
+    if (savedText) {
+      textArea.value = savedText;
+    }
+  });
+  saveButtons.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+      var textBox = textAreas[index];
+      var savedText = textBox.value;
+      // Saving it!!
+      localStorage.setItem("savedText_" + index, savedText);
+      console.log("Saved text:", savedText);
+    });
+  });
+})
